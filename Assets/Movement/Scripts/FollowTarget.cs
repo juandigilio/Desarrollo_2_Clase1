f10cb0ba4112a7800c973b32ca37ebb7bf17694b
juandigilio <106790484+juandigilio@users.noreply.github.com>
2024-03-25 12:28:58 -0300

    Added main character animations

--- Assets/Movement/Scripts/FollowTarget.cs
+++ Assets/Movement/Scripts/FollowTarget.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
 {
-
     public Transform target;
     public Vector3 offset = new Vector3 (0.0f, 1.5f, -5.0f);
+    public float rotationSpeed = 2.0f;
+    public float followSppeed = 2.0f;
+
 
     private void LateUpdate()
     {
-        transform.position = target.position + offset;
+        var rotatedOffset = target.rotation * offset;
+        var offsetEmulatingTransformPoint = target.position + rotatedOffset;
+
+        var offsetBasedOnTarget = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, offsetEmulatingTransformPoint, Time.deltaTime * followSppeed);
+
+        var desiredRotation = target.rotation * Quaternion.Euler(transform.rotation.eulerAngles.x, 0, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
     }
 }
